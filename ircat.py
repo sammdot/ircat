dbbba106839cda33c35a6ee97851b6348abde049
Sammi Fernandez <sammii.fernandez@gmail.com>
2014-09-06 13:38:18 -0400

    main loop

--- ircat.py
+++ ircat.py
@@ -12,10 +12,23 @@ def connect(host, port):
 	except socket.error as e:
 		sys.stderr.write(e + "\n")
 
-def listen(sock):
+def send(sock, msg):
+	sock.sendall(bytes(msg + "\r\n", "UTF-8"))
+
+def listen(sock, callback):
 	f = sock.makefile("rb")
 	while True:
 		msg = f.readline().decode("utf-8", errors="ignore").rstrip("\r\n")
+		print(msg)
+		parts = msg.split(" ")
+		parts = parts[1:] if parts[0].startswith(":") else parts
+		cmd = parts[0]
+		if cmd == "PING":
+			send(sock, "PONG " + parts[1])
+		elif cmd == "ERROR":
+			exit(0)
+		elif cmd == "001":
+			callback()
 
 def main():
 	pass
